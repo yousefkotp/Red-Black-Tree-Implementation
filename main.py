1ab0ec8dd57be5d2589cbaa823368ae06e8bcd6a
Yousef Kotp <yousefkotp@outlook.com>
2022-05-03 03:57:32 +0200

    Added few functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 class Node:
     #if color = 0 then red, if 1--> black
-    def __init__(self, key):        #self refrence the current instance of this class
-        self.key=key
+    def __init__(self, key):        #self references the current instance of this class
+        self.key=key                #needs a key to be initialized
         self.right=None
         self.left=None
         self.color=0
@@ -23,4 +23,32 @@ class Node:
         if self.parent.key is None:
             msg = "This node is the Root\n" + msg
         return msg
+class RedBlackTree:
+    def __init__(self,newNode=Node):        #needs a node to be initialized
+        self.nil=newNode(0)
+        self.nil.color = 1                  #the root and the nil are black
+        self.root = self.nil
+        self.number_of_nodes= 0
 
+    def insertNode(self,key):
+        newNode =Node(key)
+        node = self.root
+        parent=self.nil         #aka None aka nil
+        while(node!=self.nil):  #as long as we didn't reach the end of the tree
+            parent = node
+            if newNode.key <node.key:
+                node = node.left
+            else:
+                node = node.right
+
+        if parent == self.nil:     #if the inserted node is the first node
+            self.root=newNode
+        elif newNode.key <parent.key:
+            parent.left=newNode
+        else:
+            parent.right=newNode
+
+        self.insertFix(newNode)
+        self.number_of_nodes= self.number_of_nodes+1
+
+        def insertFix(self, newNode=Node):
